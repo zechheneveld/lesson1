68beb5ced2d9d9f10ec737de8a1db7579e484ea6
Zech Heneveld <zechheneveld@Zechs-MacBook-Air.local>
2019-06-18 14:14:54 -0700

    saved updated

--- src/main/java/com/example/demo/DataController.java
+++ src/main/java/com/example/demo/DataController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import java.time.LocalTime;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,4 +16,8 @@ public class DataController {
 	public String getTime() {
 		return "The current time is: " + LocalTime.now().toString();
 }
+	@GetMapping("{name}")
+	public String sayHello(@PathVariable String name) {
+		return "Hello " + name;
+	}
 }
