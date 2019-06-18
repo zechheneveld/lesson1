f7f8e9f24f2debee830b781f2b6bd61d42f3fcc8
Zech Heneveld <zechheneveld@Zechs-MacBook-Air.local>
2019-06-18 16:07:51 -0700

    adding newyear

--- src/main/java/com/example/demo/DataController.java
+++ src/main/java/com/example/demo/DataController.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import java.time.LocalTime;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +16,12 @@ public class DataController {
 	public String getTime() {
 		return "The current time is: " + LocalTime.now().toString();
 }
-	@GetMapping("{name}")
-	public String sayHello(@PathVariable String name) {
-		return "Hello " + name;
+//	@GetMapping("{name}")
+//	public String sayHello(@PathVariable String name) {
+//		return "Hello " + name;
+//	}
+	@GetMapping("/newyear")
+	public String getNewYear() {
+		return LocalTime.now().toString() + " days until the new year";
 	}
 }
